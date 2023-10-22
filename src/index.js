525d14d0abe7e7aaa266e9993b0e48f0ef0caec5
Ivoine <ivoinestrachan242@gmail.com>
2023-10-22 18:46:09 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const { App } = require('@slack/bolt');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  logs: {
+    level: 'debug', 
+  },
 });
 
 async function sendGasMessage(channelId, ts) {
