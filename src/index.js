ee48bbebe41d2a6cc2fe2b9ec7abe26038d0f95a
Ivoine <ivoinestrachan242@gmail.com>
2023-10-23 13:50:20 -0400

    a bit more testing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 require('dotenv').config();
 
-const { App } = require('@slack/bolt');
+const { App, ExpressReceiver } = require('@slack/bolt');
 
+const expressReceiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  logs: {
-    level: 'debug', 
-  },
+  receiver: expressReceiver,
+  logLevel: 'debug',
 });
+
 
 async function sendGasMessage(channelId, ts) {
   try {
     await app.client.chat.postMessage({
       token: process.env.SLACK_BOT_TOKEN,
-      channel: channelId,
+      channel: channelId || "gas-fund-dev",
       text: 'gas button:',
       thread_ts: ts,
       blocks: [
@@ -40,22 +41,15 @@ async function sendGasMessage(channelId, ts) {
   }
 }
 
-const channelId = 'gas-fund-dev';
+app.message(async ({ message }) => {
+  console.log('Received message:', message.text, 'in channel', message.channel);
 
-app.action('gas_button_click', async ({ ack, body }) => {
-  ack();
-  sendGasMessage(channelId);
-});
-
-app.event("message", async ({ event }) => {
-  if (event.text.contains("gas && event.channel_id === channelId")) {
-    sendGasMessage(channelId, event.ts);
+  if (message.text && message.text.toLowerCase().includes('gas')) {
+    sendGasMessage(message.channel, message.ts);
   }
 });
 
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
-
-
 })();
